12f485976e99c1bcb7dcbd001399134384994339
추교정 <58337059+cnrywjd11@users.noreply.github.com>
2026-03-14 15:28:45 +0900

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- Problem-Solving/LeetCode/0001-two-sum/0001-two-sum.cpp
+++ Problem-Solving/LeetCode/0001-two-sum/0001-two-sum.cpp
@@ -2,13 +2,17 @@ class Solution {
 public:
     vector<int> twoSum(vector<int>& nums, int target) {
         vector<int> results(2);
+        bool found = false;
         for(int i=0;i<nums.size()-1;i++) {
             for(int j=i+1;j<nums.size();j++) {
                 if(nums[i] + nums[j] == target) {
                     results[0] = i;
                     results[1] = j;
+                    found = true;
+                    break;
                 }
             }
+            if (found) break;
         }
         return results;
     }
