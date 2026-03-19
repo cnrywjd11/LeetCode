44bb8b83337cba95b9ea6e7a80fbe9a5ebb38d46
추교정 <58337059+cnrywjd11@users.noreply.github.com>
2026-03-19 10:31:55 +0900

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- Problem-Solving/LeetCode/0371-sum-of-two-integers/0371-sum-of-two-integers.cpp
+++ Problem-Solving/LeetCode/0371-sum-of-two-integers/0371-sum-of-two-integers.cpp
@@ -3,7 +3,8 @@ public:
     int getSum(int a, int b) {
         while (b != 0) {
             int carry = (a & b) << 1;
-            a = (a ^ b);
+            int sumWithoutCarry = (a ^ b);
+            a = sumWithoutCarry;
             b = carry;
         }
         return a;
