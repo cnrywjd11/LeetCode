9bf294b0bae9c7bd95e0937937741b2245a449bd
추교정 <58337059+cnrywjd11@users.noreply.github.com>
2026-03-20 12:51:29 +0900

    Time: 0 ms (100%), Space: 6.1 MB (65.05%) - LeetHub

--- Problem-Solving/LeetCode/0057-insert-interval/0057-insert-interval.go
+++ Problem-Solving/LeetCode/0057-insert-interval/0057-insert-interval.go
@@ -1,31 +1,25 @@
 func insert(intervals [][]int, newInterval []int) [][]int {
-    if len(intervals) == 0 {
+    n := len(intervals)
+    if n == 0 {
         return [][]int{newInterval}
     }
+    
     output := make([][]int, 0)
-    finished := false
-    for i, interval := range intervals {
-        if finished {
-            output = append(output, interval)
-            continue
-        }
-        if interval[1] < newInterval[0] {
-            output = append(output, interval)
-            if i == len(intervals) -1 {
-                output = append(output, newInterval)
-            }
-        } else if newInterval[1] < interval[0] {
-            output = append(output, newInterval, interval)
-            finished = true
-        } else {
-            minStart := min(interval[0], newInterval[0])
-            maxEnd := max(interval[1], newInterval[1])
-            newInterval[0] = minStart
-            newInterval[1] = maxEnd
-            if i == len(intervals) -1 {
-                output = append(output, newInterval)
-            }
-        }
+    idx := 0
+
+    for ;idx < n && intervals[idx][1] < newInterval[0];idx++ {
+        output = append(output, intervals[idx])  
     }
+    
+    for ;idx < n && newInterval[1] >= intervals[idx][0] ;idx++ {
+        newInterval[0] = min(intervals[idx][0], newInterval[0])
+        newInterval[1] = max(intervals[idx][1], newInterval[1])
+    }
+    output = append(output, newInterval)
+
+    for ;idx < n; idx++ {
+        output = append(output, intervals[idx])
+    }
+
     return output
 }
