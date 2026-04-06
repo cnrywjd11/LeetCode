a06b3a78f4246f66c42a19bc2f09d1e204a8e988
추교정 <58337059+cnrywjd11@users.noreply.github.com>
2026-04-06 13:31:35 +0900

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- Problem-Solving/LeetCode/0297-serialize-and-deserialize-binary-tree/0297-serialize-and-deserialize-binary-tree.go
+++ Problem-Solving/LeetCode/0297-serialize-and-deserialize-binary-tree/0297-serialize-and-deserialize-binary-tree.go
@@ -1,12 +1,3 @@
-/**
- * Definition for a binary tree node.
- * type TreeNode struct {
- *     Val int
- *     Left *TreeNode
- *     Right *TreeNode
- * }
- */
-
 type Codec struct {
     
 }
@@ -17,53 +8,56 @@ func Constructor() Codec {
 
 // Serializes a tree to a single string.
 func (this *Codec) serialize(root *TreeNode) string {
-    if root == nil {
-        return ""
-    }
-    return streamSerialize(root)
+	if root == nil {
+		return ""
+	}
+	return streamSerialize(root)
 }
 
 func streamSerialize(node *TreeNode) string {
-    if node == nil {
-        return "l"
-    }
-    cur := fmt.Sprint(node.Val)
-    left := streamSerialize(node.Left)
-    right := streamSerialize(node.Right)
-    return fmt.Sprintf("%s,%s,%s", cur, left, right)
+	cur := fmt.Sprint(node.Val)
+	var left, right string
+	if node.Left == nil {
+		left = "L"
+	} else {
+		left = streamSerialize(node.Left)
+	}
+	if node.Right == nil {
+		right = "L"
+	} else {
+		right = streamSerialize(node.Right)
+	}
+	
+	return fmt.Sprintf("%s,%s,%s", cur, left, right)
 }
+
 
 // Deserializes your encoded data to tree.
 func (this *Codec) deserialize(data string) *TreeNode {
-    if data == "" {
-        return nil
-    }
-    splitSlice := strings.Split(data, ",")
-    var idx int
-    return streamDeserialize(splitSlice, &idx)
+	if data == "" {
+  	return nil
+	}
+	dataSlice := strings.Split(data, ",")
+	var idx int
+	return StreamDeserialize(dataSlice, &idx)
 }
 
-func streamDeserialize(slice []string, idx *int) (*TreeNode) {
-    strVal := slice[*idx]
-    if strVal == "l" {
-        *idx = *idx + 1
-        return nil
-    }
-    val, _ := strconv.Atoi(strVal)
-    node := &TreeNode{
-        Val: val,
-    }
-    *idx = *idx + 1
-    
-    node.Left = streamDeserialize(slice, idx)
-    node.Right = streamDeserialize(slice, idx)
-    return node
+func StreamDeserialize(dataSlice []string, idx *int) *TreeNode {
+  // [1, 2, L, L, 3, 4, L, L, 5, L, L]
+  // [1, L, L]
+	if dataSlice[*idx] == "L" {
+		*idx = *idx + 1
+  	return nil
+	}
+	strVal := dataSlice[*idx]
+	val, _ := strconv.Atoi(strVal)
+	node := &TreeNode {
+		Val: val,
+	}
+	*idx = *idx + 1
+	
+	node.Left = StreamDeserialize(dataSlice, idx)
+	node.Right = StreamDeserialize(dataSlice, idx)
+	
+	return node
 }
-
-/**
- * Your Codec object will be instantiated and called as such:
- * ser := Constructor();
- * deser := Constructor();
- * data := ser.serialize(root);
- * ans := deser.deserialize(data);
- */
