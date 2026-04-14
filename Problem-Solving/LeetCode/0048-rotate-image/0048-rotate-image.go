9302928e00644c6a6fa9eac835cb700ee24752cb
추교정 <58337059+cnrywjd11@users.noreply.github.com>
2026-04-14 17:43:34 +0900

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- Problem-Solving/LeetCode/0048-rotate-image/0048-rotate-image.go
+++ Problem-Solving/LeetCode/0048-rotate-image/0048-rotate-image.go
@@ -1,7 +1,7 @@
 func rotate(matrix [][]int)  {
     size := len(matrix)
     slices.Reverse(matrix)
-    for i:=0;i<size;i++ {
+    for i:=0;i<size-1;i++ {
         for j:=i+1;j<size;j++ {
             matrix[i][j], matrix[j][i] = matrix[j][i], matrix[i][j]
         }
