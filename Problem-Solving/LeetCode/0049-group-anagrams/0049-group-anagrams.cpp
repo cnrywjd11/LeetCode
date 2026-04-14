515a1856f7b649dc781625969094063055d79440
추교정 <58337059+cnrywjd11@users.noreply.github.com>
2026-04-14 18:04:12 +0900

    Time: 21 ms (34.56%), Space: 25.8 MB (58.21%) - LeetHub

---
+++ Problem-Solving/LeetCode/0049-group-anagrams/0049-group-anagrams.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    vector<vector<string>> groupAnagrams(vector<string>& strs) {
+        vector<string> keys = strs;
+        for(int i=0;i<keys.size();i++) {
+            sort(keys[i].begin(), keys[i].end());
+        }
+        map<string, vector<string>> m;
+        for(int i=0;i<keys.size();i++) {
+            auto it = m.find(keys[i]);
+            m[keys[i]].push_back(strs[i]);
+        }
+        vector<vector<string>> ret;
+        for(auto it=m.begin();it!=m.end();it++) {
+            ret.push_back(it->second);
+        }
+        return ret;
+    }
+};
