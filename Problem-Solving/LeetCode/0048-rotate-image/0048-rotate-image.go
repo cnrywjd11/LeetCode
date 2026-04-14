7808f51a8b4480ec77dfb7426311964b4b6fab3d
추교정 <58337059+cnrywjd11@users.noreply.github.com>
2026-04-14 17:40:46 +0900

    Time: 0 ms (100%), Space: 4.1 MB (50.37%) - LeetHub

---
+++ Problem-Solving/LeetCode/0048-rotate-image/0048-rotate-image.go
@@ -0,0 +1,9 @@
+func rotate(matrix [][]int)  {
+    size := len(matrix)
+    slices.Reverse(matrix)
+    for i:=0;i<size;i++ {
+        for j:=i+1;j<size;j++ {
+            matrix[i][j], matrix[j][i] = matrix[j][i], matrix[i][j]
+        }
+    }
+}
